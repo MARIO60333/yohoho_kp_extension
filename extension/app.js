d88ace9a9af8fd5b205f087e024d721acbd559ed
Sergey Bulatov <bulatov.bul58@gmail.com>
2022-05-14 17:07:09 +0000

    Added comments

--- extension/app.js
+++ extension/app.js
@@ -1,19 +1,19 @@
-let urlArr = location.href.split('/');
+let urlArr = location.href.split('/');  //Get splited URL
 
-    function getKPId (arr) {
+    function getKPId (arr) {    //Function for parsing ID from URL
         let id = arr[arr.length - 2];
         return id;
     };
 
     function createButton () {
-        let elem = document.createElement('div')
+        let elem = document.createElement('div')    //Creating button
         elem.className = 'play_yohoho';
 
         elem.innerHTML = `
-            <input type="button" id="play_yohoho" value="<<< Смотреть в плеере Yohoho">
+            <input type="button" id="play_yohoho" value="<<< Смотреть в плеере Yohoho">     
         `;
 
-        let styles = document.createElement('style');
+        let styles = document.createElement('style');   //Creating Styles
         styles.innerHTML = `
             .play_yohoho {
                 position: fixed;
@@ -45,18 +45,20 @@ let urlArr = location.href.split('/');
                 padding: 30px;
             }
         `;
+        //Append styles and content
         document.head.append(styles);
 
         document.body.append(elem);
     }
 
-    function eventHandler () {
+    function eventHandler () {  //Creating event handler for button
         let elem = document.getElementById('play_yohoho');
         elem.addEventListener('click', () => {
-            let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1280,height=720`;
-            window.open(`http://localhost:8086/?id=${getKPId(urlArr)}`, 'Yohoho Kinopoisk Player', params);
+            let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1280,height=720`; // Setting up parameters for popup window
+            window.open(`http://localhost:8086/?id=${getKPId(urlArr)}`, 'Yohoho Kinopoisk Player', params);     //Open popup window with player
         });
     }
-
+    
+    //Initialize button and event handler
     createButton();
     eventHandler();
