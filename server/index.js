c7b67eba30bb73533a59ae3f9248a4613f42ca9b
Sergey Bulatov <bulatov.bul58@gmail.com>
2022-05-14 17:11:13 +0000

    Added comments

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const http = require("http");
+const http = require("http");   //Require some modules
 const { exec } = require('child_process');
 const fs = require('fs').promises;
 
+//Configure server
 const host = 'localhost';
 const port = 8086;
 
@@ -9,7 +10,7 @@ const requestListener = function (req, res) {
   fs.readFile(__dirname + "/assets" + "/player.html")
     .then(contents => {
       res.setHeader("Content-Type", "text/html");
-      res.writeHead(200);
+      res.writeHead(200); //OK
       res.end(contents);
     })
     .catch(err => {
@@ -21,9 +22,10 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
+  //There nothing useful, i try animate command prompt and i did it
     exec(`mode con:cols=40 lines=7&&color 1f`)
     let count = 0
-    setInterval(() => {
+    setInterval(() => { 
       switch (count) {
         case 0:
           process.stdout.write(`\x1b[2J\x1b[H>=========[ Kinopoisk Player ]=========<\n\\ Локальный сервер запущен. Порт: ${port} \n  Для выхода нажмите [CTRL + C] \n\n  Версия node.js: ${process.version} \n\\======================================/ \r`)
@@ -46,6 +48,4 @@ server.listen(port, host, () => {
 
       }
     }, 50)
-    console.log(` Для выхода нажмите [CTRL + C]`);
-    console.log(`\\======================================/`);
 });
