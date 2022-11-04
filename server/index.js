f4a41be3d257d0374fb2e3fa93f2c270baf6d040
Sergey Bulatov <bulatov.bul58@gmail.com>
2022-11-04 15:19:43 +1000

    Убрано окно консоли

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
-const http = require("http");   //Require some modules
-const { exec } = require('child_process');
+const http = require("http");
 const fs = require('fs').promises;
 
-//Configure server
 const host = 'localhost';
 const port = 8086;
 
@@ -10,7 +8,7 @@ const requestListener = function (req, res) {
   fs.readFile(__dirname + "/assets" + "/player.html")
     .then(contents => {
       res.setHeader("Content-Type", "text/html");
-      res.writeHead(200); //OK
+      res.writeHead(200);
       res.end(contents);
     })
     .catch(err => {
@@ -21,31 +19,4 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  //There nothing useful, i try animate command prompt and i did it
-    exec(`mode con:cols=40 lines=7&&color 1f`)
-    let count = 0
-    setInterval(() => { 
-      switch (count) {
-        case 0:
-          process.stdout.write(`\x1b[2J\x1b[H>=========[ Kinopoisk Player ]=========<\n\\ Локальный сервер запущен. Порт: ${port} \n  Для выхода нажмите [CTRL + C] \n\n  Версия node.js: ${process.version} \n\\======================================/ \r`)
-          count++
-          break;
-        case 1:
-          process.stdout.write(`\x1b[2J\x1b[H>=========[ Kinopoisk Player ]=========<\n| Локальный сервер запущен. Порт: ${port} \n  Для выхода нажмите [CTRL + C] \n\n  Версия node.js: ${process.version} \n\\======================================/ \r`)
-          count++
-          break;
-        case 2:
-          process.stdout.write(`\x1b[2J\x1b[H>=========[ Kinopoisk Player ]=========<\n/ Локальный сервер запущен. Порт: ${port} \n  Для выхода нажмите [CTRL + C] \n\n  Версия node.js: ${process.version} \n\\======================================/ \r`)
-          count++
-          break;
-        case 3:
-          process.stdout.write(`\x1b[2J\x1b[H>=========[ Kinopoisk Player ]=========<\n- Локальный сервер запущен. Порт: ${port} \n  Для выхода нажмите [CTRL + C] \n\n  Версия node.js: ${process.version} \n\\======================================/ \r`)
-          count++
-          break;
-        default:
-          count = 0
-
-      }
-    }, 50)
-});
+server.listen(port, host);
